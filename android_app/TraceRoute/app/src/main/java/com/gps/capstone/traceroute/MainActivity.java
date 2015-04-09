b8cacfaec08f9e68cc1b0930a907417a325a631d
Sean Aryana <saryana0@cs.washington.edu>
2015-04-09 12:29:04 -0700

    added open gl activity and view

--- android_app/TraceRoute/app/src/main/java/com/gps/capstone/traceroute/MainActivity.java
+++ android_app/TraceRoute/app/src/main/java/com/gps/capstone/traceroute/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gps.capstone.traceroute;
 
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
@@ -8,10 +9,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.gps.capstone.traceroute.listners.*;
+import com.gps.capstone.traceroute.listners.AccelerometerListener;
+import com.gps.capstone.traceroute.listners.BarometerListener;
+import com.gps.capstone.traceroute.listners.GravityListener;
+import com.gps.capstone.traceroute.listners.GyroscopeListner;
+import com.gps.capstone.traceroute.listners.LinearAccelerationListener;
+import com.gps.capstone.traceroute.listners.StepCounterListener;
+import com.gps.capstone.traceroute.listners.StepDetectorListener;
 
 public class MainActivity extends ActionBarActivity {
     // Tag used for logging
@@ -40,6 +49,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, OpenGL.class);
+                startActivity(i);
+            }
+        });
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
