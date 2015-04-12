5dafb2db6f85d30675ddead125f45a8fc5306f54
gammoa <gammoa@cs.washington.edu>
2015-04-12 16:51:36 -0700

    Refactor

---
+++ android_app/TraceRoute/app/src/main/java/com/gps/capstone/traceroute/GLFiles/util/TouchType.java
@@ -0,0 +1,9 @@
+package com.gps.capstone.traceroute.GLFiles.util;
+
+/**
+ * Created by gammoa on 4/12/15.
+ */
+public enum TouchType {
+    // Enumerated type to indicate what the previous touch event was.
+    SINGLE, DOUBLE, SHIT;
+}
