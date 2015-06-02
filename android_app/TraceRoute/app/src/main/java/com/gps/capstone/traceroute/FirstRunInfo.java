98c6eafd80436425cb7c604568d3afd4d031bef2
Sean Aryana <saryana0@cs.washington.edu>
2015-06-01 21:34:28 -0700

    fixed the start up screen

--- android_app/TraceRoute/app/src/main/java/com/gps/capstone/traceroute/FirstRunInfo.java
+++ android_app/TraceRoute/app/src/main/java/com/gps/capstone/traceroute/FirstRunInfo.java
@@ -4,33 +4,86 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gps.capstone.traceroute.GLFiles.OpenGLActivity;
 
 
-public class FirstRunInfo extends ActionBarActivity implements OnClickListener, OnCheckedChangeListener {
+public class FirstRunInfo extends AppCompatActivity implements OnClickListener, OnCheckedChangeListener, OnKeyListener {
 
-    private EditText mStrideLength;
+    private TextView mStrideLengthText;
+    private EditText mStrideLengthEdit;
     private EditText mHeightFt;
     private EditText mHeightIn;
     private Button mContinueButton;
-//    private Button mCalculateButton;
     private SharedPreferences mSharedPrefs;
-    // True signifies a male, false a female
-//    private Switch mGenderSwitch;
     private RadioGroup mRadioGroup;
+    private float mStrideLength;
+    private int mHeightFtVal;
+    private int mHeightInVal;
+    private int mTotalHeightVal;
+    private StrideLength mStrideType;
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        String val;
+        switch (v.getId()) {
+            // Update the view on each key press hopefully only once. unless the user is mean.
+            case R.id.info_height_feet:
+                val = mHeightFt.getText().toString();
+                if (val.length() == 0) break;
+                mHeightFtVal = Integer.valueOf(val);
+                if (mHeightFtVal > 3) {
+                    mContinueButton.setEnabled(true);
+                }
+                calculateStride();
+                break;
+            case R.id.info_height_inches:
+                val = mHeightIn.getText().toString();
+                if (val.length() == 0) break;
+                mHeightInVal = Integer.valueOf(val);
+                calculateStride();
+                break;
+            // Are we doing a manual override of the stride length
+            case R.id.calculated_stride_length_edit:
+                val = mStrideLengthEdit.getText().toString();
+                if (val.length() == 0) break;
+                mStrideLength = Float.valueOf(val);
+                if (mStrideLength > 30) {
+                    mContinueButton.setEnabled(true);
+                }
+                break;
+        }
+        return false;
+    }
+
+    private enum StrideLength {
+        MALE(.415f), FEMALE(.413f), OTHER(.414f);
+
+        private float strideLength;
+
+        StrideLength(float n) {
+            strideLength = n;
+        }
+
+        public float getStrideLength() {
+            return strideLength;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,83 +91,60 @@ public class FirstRunInfo extends ActionBarActivity implements OnClickListener, 
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         if (!mSharedPrefs.getBoolean(getString(R.string.pref_key_first_run), true)) {
             Intent i = new Intent(this, OpenGLActivity.class);
-//            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
             startActivity(i);
             finish();
             return;
         }
+
         setContentView(R.layout.activity_first_run_info);
-        mStrideLength = (EditText) findViewById(R.id.calculated_stride_length);
+
+        mStrideLength = 0;
+        mHeightInVal = 0;
+        mHeightFtVal = 0;
+        mTotalHeightVal = 0;
+        mStrideType = StrideLength.FEMALE;
+
+        mContinueButton = (Button) findViewById(R.id.continue_button);
         mHeightFt = (EditText) findViewById(R.id.info_height_feet);
         mHeightIn = (EditText) findViewById(R.id.info_height_inches);
-        mContinueButton = (Button) findViewById(R.id.continue_button);
         mRadioGroup = (RadioGroup) findViewById(R.id.gender);
-//        mCalculateButton = (Button) findViewById(R.id.calculate_button);
-//        mGenderSwitch = (Switch) findViewById(R.id.gender_switch);
-        mRadioGroup.setOnCheckedChangeListener(this);
-        mStrideLength.setText("0");
-        mHeightFt.setText("0");
-        mHeightIn.setText("0");
+        mStrideLengthEdit = (EditText) findViewById(R.id.calculated_stride_length_edit);
+        mStrideLengthText = (TextView) findViewById(R.id.calculated_stride_length_text);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mContinueButton.setOnClickListener(this);
-//        mCalculateButton.setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_first_run_info, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mRadioGroup.setOnCheckedChangeListener(this);
+        mStrideLengthText.setOnClickListener(this);
+        // Lets update the value as they are pressing the key
+        mHeightFt.setOnKeyListener(this);
+        mHeightIn.setOnKeyListener(this);
+        mStrideLengthEdit.setOnKeyListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        int heightFt = Integer.valueOf(mHeightFt.getText().toString());
-        int heightIn = Integer.valueOf(mHeightIn.getText().toString());
-        float strideLength = Float.valueOf(mStrideLength.getText().toString());
-        int totalHeight = heightFt * 12 + heightIn;
-
-        // Are we calculating the stride?
-        if (false) {
-            strideLength = calculateStride(totalHeight);
-            mStrideLength.setText(String.valueOf(strideLength));
-
-        } else {
-            // Has the user entered in the data?
-            if (heightFt != 0) {
-                if (strideLength == 0) {
-                    strideLength = calculateStride(totalHeight);
+        switch (v.getId()) {
+            case R.id.calculated_stride_length_text:
+                mStrideLengthEdit.setVisibility(View.VISIBLE);
+                mStrideLengthText.setVisibility(View.GONE);
+                break;
+            case R.id.continue_button:
+                if (mStrideLength < 20) {
+                    Toast.makeText(FirstRunInfo.this, "Please enter a valid height", Toast.LENGTH_SHORT).show();
+                } else {
+                    mSharedPrefs.edit()
+                            .putString(getString(R.string.pref_key_height_ft), "" + mHeightFtVal)
+                            .putString(getString(R.string.pref_key_height_in), "" + mHeightInVal)
+                            .putInt(getString(R.string.pref_key_total_height_in), mTotalHeightVal)
+                            .putFloat(getString(R.string.pref_key_stride_length), mStrideLength)
+                            .apply();
+                    Intent i = new Intent(this, OpenGLActivity.class);
+                    startActivity(i);
                 }
-                mSharedPrefs.edit()
-                        .putString(getString(R.string.pref_key_height_ft), "" + heightFt)
-                        .putString(getString(R.string.pref_key_height_in), "" + heightIn)
-                        .putInt(getString(R.string.pref_key_total_height_in), totalHeight)
-                        .putFloat(getString(R.string.pref_key_stride_length), strideLength)
-                        .apply();
-                Intent i = new Intent(this, OpenGLActivity.class);
-                startActivity(i);
-            } else {
-                Toast.makeText(this, "Must Enter correct height", Toast.LENGTH_LONG).show();
-            }
         }
     }
 
@@ -123,31 +153,27 @@ public class FirstRunInfo extends ActionBarActivity implements OnClickListener, 
      *
      * https://www.walkingwithattitude.com/articles/features/how-to-measure-stride-or-step-length-for-your-pedometer
      * Men ~ .415 Women ~.413 => this only really matters if we want to display distance traveled
-     *
-     * @param totalHeight Height of user in inches
-     * @return Stride length in inches
      */
-    private float calculateStride(int totalHeight) {
-        float strideLength;
-        // Is this a male?
-//        if (mGenderSwitch.isChecked()) {
-            strideLength = .415f * totalHeight;
-//        } else {
-//            strideLength = .413f * totalHeight;
-//        }
-//        Log.d("STRIDE LEN", "Male true Female false: " + mGenderSwitch.isChecked() + " stride length " + strideLength);
-        return strideLength;
+    private void calculateStride() {
+        mTotalHeightVal = mHeightFtVal * 12 + mHeightInVal;
+        mStrideLength = mTotalHeightVal * mStrideType.getStrideLength();
+        mStrideLengthText.setText(String.valueOf(mStrideLength));
+        mStrideLengthEdit.setText(String.valueOf(mStrideLength));
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
         switch (checkedId) {
             case R.id.female:
+                mStrideType = StrideLength.FEMALE;
                 break;
             case R.id.male:
+                mStrideType = StrideLength.MALE;
                 break;
             case R.id.other:
+                mStrideType = StrideLength.OTHER;
                 break;
         }
+        calculateStride();
     }
 }
